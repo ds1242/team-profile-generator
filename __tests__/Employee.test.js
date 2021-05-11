f42ff029467b4e192abf60fda67ea25981e6bb1e
dshaw1242 <david.shaw1242@gmail.com>
2021-05-11 07:23:44 -0600

    finished up employee object tests and class methods

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -25,3 +25,9 @@ test('get employee email', () => {
 
     expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email));
 })
+
+test("get employee's role", () =>{
+    const employee = new Employee('David', '117', 'email@email.com');
+
+    expect(employee.getRole()).toEqual(expect.stringContaining(employee.role));
+})
