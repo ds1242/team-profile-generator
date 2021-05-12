e9a27b05b08c35b3204bbf7804e27231b34595e5
dshaw1242 <david.shaw1242@gmail.com>
2021-05-12 06:57:28 -0600

    setup mock for both manager and engineer to be separate

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,12 +1,20 @@
 const Employee = require('../lib/Employee');
+const Engineer = require('../lib/Engineer');
 
 // mock's the data
-jest.mock('../lib/Employee')
+jest.mock('../lib/Engineer')
 
 test('tests engineer object creation', () => {
     const engineer = new Engineer()
 
-    expect(manager.name).toBe('David');
-    expect(manager.id).toBe(117);
-    expect(manager.email).toBe('email@email.com');
+    expect(engineer.name).toBe('David');
+    expect(engineer.id).toBe(117);
+    expect(engineer.email).toBe('email@email.com');
+
+    console.log(engineer);
 })
+
+test("get engineer's role", () =>{
+    const engineer = new Engineer();
+    expect(engineer.getRole()).toEqual(expect.stringContaining('Engineer'));
+});
