3df37f12b42aa32372cdbd79c000ce30e1620207
dshaw1242 <david.shaw1242@gmail.com>
2021-05-11 21:09:22 -0600

    create Manager object and set role to Manager

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,9 +1,24 @@
 const Employee = require('../lib/Employee.js');
 const Manager = require('../lib/Manager.js');
 
+// mock's the data???
+jest.mock('../lib/__mocks__/Employee')
+
 test('creates manager object', () => {
 
-    const manager = new Employee('David', 117, 'email@email.com', 'Manager')
+    const manager = new Manager('David', 117, 'email@email.com', 'Manager')
+    expect(manager.name).toBe('David');
+    expect(manager.id).toBe(117);
+    expect(manager.email).toBe('email@email.com');
 
+    console.log(manager);
+    
+});
+
+test("get managers's role", () =>{
+    const manager = new Manager('David', 117, 'email@email.com', 'Manager');
+    // console.log(`the role is ${manager.role}`);
     expect(manager.getRole()).toEqual(expect.stringContaining('Manager'));
-})
+
+    console.log(manager.getRole());
+});
