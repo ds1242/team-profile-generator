af779a5e28961fccf814a53a93674ac733a92c22
dshaw1242 <david.shaw1242@gmail.com>
2021-05-13 12:54:48 -0600

    take in input from user, add to array of items

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Choices = require('inquirer/lib/objects/choices');
 
+const employeeArr = [];
+
+
 // create array of questions for employees
 const questions = [
     {
@@ -27,28 +30,63 @@ const questions = [
         name: 'role',
         message: "Please select this employee's role",
         choices: ['Manager', 'Engineer', 'Intern']
+    },
+    {
+        type: 'confirm',
+        name: 'confirmAddEmployee',
+        message: 'Would you like to enter another employee?',
+        default: false
     }
+    
 ];
+const addAdditionalEmployees = () => {
+    inquirer
+        .prompt([
+            
+        ])
+        
+    
+}
+
 
 const promptUser = () => {
+    
     return inquirer
         .prompt(questions)
-        .then(({name, id, email, role}) => {
+        .then(({name, id, email, role, confirmAddEmployee}) => {
             if(role === 'Manager') {
                 this.employee = new Manager(name, id, email, role);
                 this.employee.getRole();
-                console.log(this.employee);
+                employeeArr.push(this.employee);
+                console.log(employeeArr);
+                if(confirmAddEmployee) {
+                    return promptUser();
+                } else {
+                    return employeeArr;
+                }
             } else if(role === 'Engineer') {
                 this.employee = new Engineer(name, id, email, role);
                 this.employee.getRole();
-                console.log(this.employee);
+                employeeArr.push(this.employee);                
+                console.log(employeeArr);
+                if(confirmAddEmployee) {
+                    return promptUser();
+                } else {
+                    return employeeArr;
+                }
             } else if(role === 'Intern') {
                 this.employee = new Intern(name, id, email, role);
                 this.employee.getRole();
-                console.log(this.employee);
+                employeeArr.push(this.employee);
+                console.log(employeeArr);
+                if(confirmAddEmployee) {
+                    return promptUser();
+                } else {
+                    return employeeArr;
+                }
             }            
         })
-}
-
+       
+    }
 
 promptUser();
